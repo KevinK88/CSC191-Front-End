276192059f10efbc0d34696c631b9d0b6131ae78
Paul Davydov <sirruskilotpaul@gmail.com>
2020-09-25 15:45:12 -0700

    Pushing a few changes

--- src/components/subTasks.jsx
+++ src/components/subTasks.jsx
@@ -12,7 +12,9 @@ class SubTasks extends Component {
     priority: "",
     parentTaskId: "",
     dueDate: {},
+    tasks: [],
     subTasks: [],
+    expanded: false,
   };
 
   async componentDidMount() {
@@ -27,7 +29,7 @@ class SubTasks extends Component {
           taskId: res.data.taskId,
           taskDescription: res.data.taskDescription,
           dueDate: res.data.dueDate,
-          subTasks: res.data.subTasks,
+          tasks: res.data.subTasks,
           priority: res.data.priority,
           completed: res.data.completed,
           parentTaskId: res.data.parentTaskId,
@@ -52,7 +54,7 @@ class SubTasks extends Component {
             taskId: res.data.taskId,
             taskDescription: res.data.taskDescription,
             dueDate: res.data.dueDate,
-            subTasks: res.data.subTasks,
+            tasks: res.data.subTasks,
             priority: res.data.priority,
             parentTaskId: res.data.parentTaskId,
             completed: res.data.completed,
@@ -71,8 +73,8 @@ class SubTasks extends Component {
         .delete(`/project/${this.state.projectId}/task/${task.taskId}`)
         .then((res) => {})
         .catch((err) => console.log(err));
-      const tasks = this.state.subTasks.filter((t) => t.taskId !== task.taskId);
-      this.setState({ subTasks: tasks });
+      const tasks = this.state.tasks.filter((t) => t.taskId !== task.taskId);
+      this.setState({ tasks });
     }
   };
 
@@ -99,6 +101,74 @@ class SubTasks extends Component {
     // console.log(dueDate, today);
     return Math.floor(
       (dueDate.getTime() - today.getTime()) / (1000 * 3600 * 24)
+    );
+  };
+
+  getSubtasks = (task) => {
+    if (this.state.expanded === false) {
+      axios
+        .get(`/project/${this.state.projectId}/task/${task.taskId}`)
+        .then((res) => {
+          this.setState({ subTasks: res.data.subTasks, expanded: true });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else if (this.state.expanded === true) {
+      this.setState({ expanded: false });
+    }
+    console.log(this.state.tasks, this.state.expanded);
+  };
+
+  renderRow = (task) => {
+    return (
+      <tr key={task.taskId}>
+        <td>{task.priority}</td>
+        <td>
+          <Link to={`/project/${this.state.projectId}/task/${task.taskId}`}>
+            {task.taskName}
+          </Link>
+        </td>
+        <td>{task.taskDescription}</td>
+
+        <td>{task.subTaskCount} Tasks</td>
+        <td>{this.getDaysLeft(task.dueDate)} Days Left</td>
+        {!task.completed && <td style={{ color: "#e57373" }}>Incomplete</td>}
+        {task.completed && <td style={{ color: "green" }}>Complete</td>}
+        <td className="d-flex justify-content-end">
+          <Link
+            to={{
+              pathname: `/project/${this.state.projectId}/task/${task.taskId}/edit`,
+              state: {
+                parentTaskId: task.parentTaskId,
+                taskId: task.taskId,
+                taskName: task.taskName,
+                taskDescription: task.taskDescription,
+                priority: task.priority,
+                dueDate: task.dueDate,
+                completed: task.completed,
+                type: "edit sub",
+              }, //
+            }}
+            className="btn btn-primary btn-sm mr-2"
+          >
+            Edit
+          </Link>
+          <button
+            onClick={() => this.handleDelete(task)}
+            className="btn btn-danger btn-sm"
+          >
+            Delete
+          </button>
+          <button
+            onClick={() => this.getSubtasks(task)}
+            className="btn btn-secondary btn-sm"
+            style={{ marginLeft: 7 }}
+          >
+            Expand
+          </button>
+        </td>
+      </tr>
     );
   };
 
@@ -164,55 +234,18 @@ class SubTasks extends Component {
           className="table"
           style={{ color: "white", textDecoration: "none" }}
         >
-          <tbody>
-            {this.state.subTasks.map((task) => (
-              <tr key={task.taskId}>
-                <td>{task.priority}</td>
-                <td>
-                  <Link
-                    to={`/project/${this.state.projectId}/task/${task.taskId}`}
-                  >
-                    {task.taskName}
-                  </Link>
-                </td>
-                <td>{task.taskDescription}</td>
-                {/*<td>SubTasks: {task.subTaskCount}</td>*/}
-                <td>{task.subTaskCount} Tasks</td>
-                <td>{this.getDaysLeft(task.dueDate)} Days Left</td>
-                {!task.completed && (
-                  <td style={{ color: "#e57373" }}>Incomplete</td>
-                )}
-                {task.completed && <td style={{ color: "green" }}>Complete</td>}
-                <td className="d-flex justify-content-end">
-                  <Link
-                    to={{
-                      pathname: `/project/${this.state.projectId}/task/${task.taskId}/edit`,
-                      state: {
-                        parentTaskId: task.parentTaskId,
-                        taskId: task.taskId,
-                        taskName: task.taskName,
-                        taskDescription: task.taskDescription,
-                        priority: task.priority,
-                        dueDate: task.dueDate,
-                        completed: task.completed,
-                        type: "edit sub",
-                      }, //
-                    }}
-                    className="btn btn-primary btn-sm mr-2"
-                  >
-                    Edit
-                  </Link>
-                  <button
-                    onClick={() => this.handleDelete(task)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          <tbody>{this.state.tasks.map((task) => this.renderRow(task))}</tbody>
         </table>
+        {this.state.expanded && (
+          <table
+            className="table"
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            <tbody>
+              {this.state.subTasks.map((subTask) => this.renderRow(subTask))}
+            </tbody>
+          </table>
+        )}
       </React.Fragment>
     );
   }
--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -7,6 +7,8 @@ class Tasks extends Component {
     projectId: "",
     projectName: "",
     tasks: [],
+    subTasks: [],
+    expanded: false,
   };
 
   async componentDidMount() {
@@ -35,12 +37,79 @@ class Tasks extends Component {
     }
   };
 
+  getSubtasks = (task) => {
+    if (this.state.expanded === false) {
+      axios
+        .get(`/project/${this.state.projectId}/task/${task.taskId}`)
+        .then((res) => {
+          this.setState({ subTasks: res.data.subTasks, expanded: true });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else if (this.state.expanded === true) {
+      this.setState({ expanded: false });
+    }
+    console.log(this.state.subTasks, this.state.expanded);
+  };
+
   getDaysLeft = (dueDateString) => {
     var dueDate = new Date(dueDateString);
     var today = new Date(Date.now());
     // console.log(dueDate, today);
     return Math.floor(
       (dueDate.getTime() - today.getTime()) / (1000 * 3600 * 24)
+    );
+  };
+
+  renderRow = (task) => {
+    return (
+      <tr key={task.taskId}>
+        <td>{task.priority}</td>
+        <td>
+          <Link to={`/project/${this.state.projectId}/task/${task.taskId}`}>
+            {task.taskName}
+          </Link>
+        </td>
+        <td>{task.taskDescription}</td>
+        <td>{task.subTaskCount} Tasks</td>
+        <td>{this.getDaysLeft(task.dueDate)} Days Left</td>
+        {!task.completed && <td style={{ color: "#e57373" }}>Incomplete</td>}
+        {task.completed && <td style={{ color: "green" }}>Complete</td>}
+        <td className="d-flex justify-content-end">
+          <Link
+            to={{
+              pathname: `/project/${this.state.projectId}/task/${task.taskId}/edit`,
+              state: {
+                parentTaskId: task.parentTaskId,
+                taskId: task.taskId,
+                taskName: task.taskName,
+                taskDescription: task.taskDescription,
+                priority: task.priority,
+                completed: task.completed,
+                dueDate: task.dueDate,
+                type: "edit main",
+              },
+            }}
+            className="btn btn-primary btn-sm mr-2"
+          >
+            Edit
+          </Link>
+          <button
+            onClick={() => this.handleDelete(task)}
+            className="btn btn-danger btn-sm"
+          >
+            Delete
+          </button>
+          <button
+            onClick={() => this.getSubtasks(task)}
+            className="btn btn-secondary btn-sm"
+            style={{ marginLeft: 7 }}
+          >
+            Expand
+          </button>
+        </td>
+      </tr>
     );
   };
 
@@ -64,54 +133,18 @@ class Tasks extends Component {
           className="table"
           style={{ color: "white", textDecoration: "none" }}
         >
-          <tbody>
-            {this.state.tasks.map((task) => (
-              <tr key={task.taskId}>
-                <td>{task.priority}</td>
-                <td>
-                  <Link
-                    to={`/project/${this.state.projectId}/task/${task.taskId}`}
-                  >
-                    {task.taskName}
-                  </Link>
-                </td>
-                <td>{task.taskDescription}</td>
-                <td>{task.subTaskCount} Tasks</td>
-                <td>{this.getDaysLeft(task.dueDate)} Days Left</td>
-                {!task.completed && (
-                  <td style={{ color: "#e57373" }}>Incomplete</td>
-                )}
-                {task.completed && <td style={{ color: "green" }}>Complete</td>}
-                <td className="d-flex justify-content-end">
-                  <Link
-                    to={{
-                      pathname: `/project/${this.state.projectId}/task/${task.taskId}/edit`,
-                      state: {
-                        parentTaskId: task.parentTaskId,
-                        taskId: task.taskId,
-                        taskName: task.taskName,
-                        taskDescription: task.taskDescription,
-                        priority: task.priority,
-                        completed: task.completed,
-                        dueDate: task.dueDate,
-                        type: "edit main",
-                      },
-                    }}
-                    className="btn btn-primary btn-sm mr-2"
-                  >
-                    Edit
-                  </Link>
-                  <button
-                    onClick={() => this.handleDelete(task)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
+          <tbody>{this.state.tasks.map((task) => this.renderRow(task))}</tbody>
         </table>
+        {this.state.expanded && (
+          <table
+            className="table"
+            style={{ color: "white", textDecoration: "none" }}
+          >
+            <tbody>
+              {this.state.subTasks.map((subTask) => this.renderRow(subTask))}
+            </tbody>
+          </table>
+        )}
       </React.Fragment>
     );
   }
